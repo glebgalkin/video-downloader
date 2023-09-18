09091e3abdb33341c4635d500f7bd8027846661e
Gleb Galkin <gleb.galki@bellmedia.ca>
2023-09-17 22:47:13 -0400

    Added vitest config

---
+++ backend/tests/main.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, it} from "vitest";
+
+describe('Temporary test', () => {
+    it('Some test', () => {
+        const stringer = "some string"
+        expect(stringer).toEqual("some string")
+    })
+})
